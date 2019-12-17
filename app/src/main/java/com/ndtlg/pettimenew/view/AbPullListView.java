7edf9f07931d3d4dc009e9458e786ac12cd545d7
daif <daif@deepblueai.com>
2019-12-17 15:03:46 +0800

    0

--- app/src/main/java/com/ndtlg/pettimenew/view/AbPullListView.java
+++ app/src/main/java/com/ndtlg/pettimenew/view/AbPullListView.java
@@ -11,7 +11,7 @@ package com.ndtlg.pettimenew.view;/*
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.l
  */
 
 import android.content.Context;
